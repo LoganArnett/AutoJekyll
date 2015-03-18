264d425361a03c334ba198f52e5b0ed1bc1b7c8b
LoganArnett <Loganarn5@gmail.com>
2015-03-18 18:51:42 -0400

    markdown set up with live preview and base64 installed

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('autoJekyll', ['ngAnimate', 'ngSanitize', 'restangular', 'ui.router', 'ui.bootstrap'])
+angular.module('autoJekyll', ['ngAnimate', 'ngSanitize', 'restangular', 'ui.router', 'ui.bootstrap', 'yaru22.md'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl as app'
       });
 
     $urlRouterProvider.otherwise('/');
