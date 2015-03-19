fb8452c9193c7636626325f58324b3cb96ea3c99
LoganArnett <Loganarn5@gmail.com>
2015-03-19 18:25:58 -0400

    updating README.md and styled the main landing page

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -17,37 +17,45 @@ angular.module('autoJekyll')
         year: '2015'
     }
    
-
-//https://api.github.com/repos/loganarnett/LoganArnett.github.io/contents/_posts/2014-09-23-The-Iron-Blog-Has-Landed.md
-//    customPUT()
     this.gitPub = function(post){
-        this.postMonth = post.month.toString()
-        this.postDay = post.day.toString()
-//        day = date.length == 1 ? "0" + date : date;
+//        Convert numbers returned into strings
+        this.postMonth = post.month.toString();
+        this.postDay = post.day.toString();
+        this.postYear = post.year.toString();
+        
+//      Check for proper date formats for Month and Day
       if(this.postMonth.length == 1){
           this.postMonth = '0' + this.postMonth;
-          console.log(this.postMonth)
       }
       if(this.postDay.length == 1){
           this.postDay = '0' + this.postDay;
-          console.log(this.postDay)
       }
+//        Replace all spaces in the users title with dashes
       this.fileName = post.fileName.replace(/ /g, '-');
+        
+//        Created the properly structure markdown file name for the post
       this.datedFile = this.postYear + '-' + this.postMonth + '-' + this.postDay + '-' + this.fileName + '.md';
+        
+//        Grab the commit message    
       this.cMessage = post.cMessage;
+        
+//        Base64 encode the actual post content as required by Github
       this.encoded = $base64.encode(post.content)
-      console.log(this.encoded)
-      
+
+//        Using Restangular to create a customPUT with the required params requested by Github
 //      Restangular.one('_posts/').customPUT( {"content": this.encoded, "message": this.cMessage}, this.datedFile, {}, {"Authorization": 'token 6c5e78e3ac7a7571b0f1f32d07034a97434b8b88'} )
       
-      return this.newPost = {
+//      Reset the newPost object back to its original blank state upon submission of a post
+      return self.newPost = {
         fileName: '',
-        content: this.template,
+        content:  self.template,
         cMessage: '',
-        postDate: ''
-    };
-      
+        month: 1,
+        day: 1,
+        year: '2015'
     }
+      
+    }//END This.gitPub Function
     
 // Making the date selection less error prone
     this.month = 12;
